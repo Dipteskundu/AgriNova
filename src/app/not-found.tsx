89154fbec6c0c5c8613335454abf4d81f94e5d8a
muhiuddinshanto <muhiuddinshanto@gmail.com>
2026-09-22 08:04:51 +0600

    Resolve marge conflicts with development

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
 import { tr } from "@/agriplatform/lib/localize";
-=======
->>>>>>> origin/development
+
 export default function NotFound() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-6xl font-bold">404</h1>
-<<<<<<< HEAD
-      <p className="mt-4 text-xl">{tr('Page Not Found')}</p>
-      <a href="/" className="mt-6 text-blue-500 hover:underline">{tr('Go back home')}</a>
-=======
-      <p className="mt-4 text-xl">Page Not Found</p>
+      <p className="mt-4 text-xl">{tr("Page Not Found")}</p>
       <a href="/" className="mt-6 text-blue-500 hover:underline">
-        Go back home
+        {tr("Go back home")}
       </a>
->>>>>>> origin/development
     </div>
   );
 }
